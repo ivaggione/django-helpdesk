997a85c4bf610fa04ca59dcf544215ed312fe994
Ignacio Vaggione <ivaggione@gmail.com>
2024-05-03 16:58:51 +0200

    Django4 remove decorator attrs

--- helpdesk/decorators.py
+++ helpdesk/decorators.py
@@ -4,8 +4,6 @@ from django.core.exceptions import PermissionDenied
 from django.http import Http404
 from django.shortcuts import redirect
 
-from django.utils.decorators import available_attrs
-
 
 from helpdesk import settings as helpdesk_settings
 
@@ -15,7 +13,7 @@ def protect_view(view_func):
     Decorator for protecting the views checking user, redirecting
     to the log-in page if necessary or returning 404 status code
     """
-    @wraps(view_func, assigned=available_attrs(view_func))
+    @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if not request.user.is_authenticated and helpdesk_settings.HELPDESK_REDIRECT_TO_LOGIN_BY_DEFAULT:
             return redirect('helpdesk:login')
@@ -31,7 +29,7 @@ def staff_member_required(view_func):
     Decorator for staff member the views checking user, redirecting
     to the log-in page if necessary or returning 403
     """
-    @wraps(view_func, assigned=available_attrs(view_func))
+    @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if not request.user.is_authenticated and not request.user.is_active:
             return redirect('helpdesk:login')
@@ -47,7 +45,7 @@ def superuser_required(view_func):
     Decorator for superuser member the views checking user, redirecting
     to the log-in page if necessary or returning 403
     """
-    @wraps(view_func, assigned=available_attrs(view_func))
+    @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
         if not request.user.is_authenticated and not request.user.is_active:
             return redirect('helpdesk:login')
